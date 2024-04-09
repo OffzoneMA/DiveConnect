3393a0f161dd9bf622d6f6bd735a95dcc5901b96
Youssef DIOURI <youssefdiouri4@gmail.com>
2024-04-10 01:39:14 +0200

    making vercel work

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const listEndpoints = require('express-list-endpoints');
+
+const divingAssociationRoutes = require('./microservices/divingAssociation/routes/divingAssociation');
+const bookingRoutes = require('./src/microservices/booking/routes/booking');
+const userRoutes = require('./src/microservices/user/routes/userRouter');
+const divingCenterRoutes = require('./src/microservices/divingCenter/routes/divingCenter');
+
+
+dotenv.config();
+
 const app = express();
-const divingCenterRoutes = require('./routes/diving-center');
-const divingAssociationRoutes = require('./routes/diving-association');
-const userRoutes = require('./routes/user');
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(express.json());
-app.use('/api/diving-center', divingCenterRoutes);
-app.use('/api/diving-association', divingAssociationRoutes);
-app.use('/api/user', userRoutes);
+// Use routes
+app.use('/api/diving-centers', divingCenterRoutes);
+app.use('/api/diving-associations', divingAssociationRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/users', userRoutes);
+app.use('/', (req, res) => {
+  const endpoints = listEndpoints(app);
+  res.json(endpoints);
+});
 
-const port = process.env.PORT || 3001;
+// Connect to database
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
+
+// Start server
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
